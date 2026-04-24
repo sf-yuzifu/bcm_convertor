4514ba0ca12773a2640337d53802c9a66810d12e
sf-yuzifu <yuzifu@yuzifu.top>
2026-04-24 20:52:49 +0800

    [refactor]移除已弃用的函数模块

--- src/functions/convert.js
+++
@@ -1,4 +0,0 @@
-export { loadOfflineKitten3Project as getK3file } from '../services/projectSources/adapters/offlineProjectSourceAdapter.js'
-export { loadOnlineProject as getOnlineInfo } from '../services/projectSources/onlineProjectSourceGateway.js'
-export { online, kitten3, kitten4 } from './templates.js'
-export { macos, linux, windows } from './packagers.js'
